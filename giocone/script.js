6a0f76eef89a1a899bbde804180331ede4737558
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2021-12-02 10:52:31 +0000

    siuuuuuu

---
+++ giocone/script.js
@@ -0,0 +1,33 @@
+function startGame() {
+    myGameArea.start();
+   
+        myGameArea.draw(redSquare);
+      }
+      
+      
+
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+
+    draw: function(component) {
+        this.context.fillStyle = component.color;
+        this.context.fillRect(component.x, component.y, component.width, component.height);
+      }
+    
+}
+
+var redSquare = {
+    width: 20,
+    height: 20,
+    x: 10,
+    y: 120,
+    color: "red"
+  },
+  
