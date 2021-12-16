e2de3e6395e1d964207a50e6858fa346f9b2b174
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2021-12-16 13:52:00 +0000

    sium

--- giocone/script.js
+++ giocone/script.js
@@ -1,14 +1,15 @@
 function startGame() {
   myGameArea.start();
-  myGameArea.draw(redSquare);
   animatedObject.loadImages();
+  
+  
 }
 function updateGameArea() {
   myGameArea.wallpaper(wallpaper);
-  redSquare.x += redSquare.speedX;
-  redSquare.y += redSquare.speedY;
-
-  myGameArea.draw(redSquare);
+ 
+  myGameArea.clear();
+  animatedObject.update();
+ 
   myGameArea.drawGameObject(animatedObject);
   animatedObject.update();
 }
@@ -16,8 +17,8 @@ var myGameArea = {
 
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 800;
-    this.canvas.height = 800;
+    this.canvas.width = 1900;
+    this.canvas.height = 948;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 10);
@@ -38,21 +39,16 @@ var myGameArea = {
       gameObject.y,
       gameObject.width,
       gameObject.height
-    );
-
+    )
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+}
 
 
-}
-var redSquare = {
-  width: 50,
-  height: 50,
-  x: 10,
-  y: 120,
-  color: "red",
-  speedX: 0,
-  speedY: 0
-}
+
+
 var wallpaper = {
   width: 1200,
   height: 4000,
@@ -60,23 +56,6 @@ var wallpaper = {
   y: 0,
   color: "#f1f1f1"
 }
-let v = 1
-function moveup() {
-  redSquare.speedY -= v;
-}
-
-function movedown() {
-  redSquare.speedY += v;
-}
-
-function moveleft() {
-  redSquare.speedX -= v;
-}
-
-function moveright() {
-  redSquare.speedX += v;
-}
-
 var animatedObject = {
   speedX: 0,
   speedY: 0,
@@ -84,12 +63,73 @@ var animatedObject = {
   height: 60,
   x: 10,
   y: 120,
+  imageList: [], //Vettore che conterrà tutte le immagini caricate
+  contaFrame: 0, //Tiene conto di quanti frame sono passati
+  actualFrame: 0, //Specifica quale frame disegnare
+
+  update: function () {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.contaFrame++;
+    if (this.contaFrame == 10) {
+      this.contaFrame = 0;
+      this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+      //console.log(this.actualFrame);
+      this.image = this.imageList[this.actualFrame];
+    }
+  },
 
   loadImages: function () {
-    this.image = new Image(this.width, this.height);
-    this.image.src = "https://i.ibb.co/M7WMMSF/Run-000.png"; //Qui metti una tua immagine
+    
+    for (imgPath of running) {
+      var img = new Image(this.width, this.height);
+      img.src = imgPath;
+      console.log(img);
+      this.imageList.push(img);
+      //console.log(img);
+    }
+    this.image = this.imageList[this.actualFrame];
   }
 };
+
+
+let v = 2
+
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "a") {
+    animatedObject.speedX = -v;
+    
+  }
+  if (event.key == "d") {
+    animatedObject.speedX = v;
+  }
+  if (event.key == "w") {
+    animatedObject.speedY = -v;
+  }
+  if (event.key == "s") {
+    animatedObject.speedY = v;
+  }
+})
+document.addEventListener("keyup", (event) => {
+  if (event.key == "a") {
+    animatedObject.speedX = 0;
+
+  }
+  if (event.key == "d") {
+    animatedObject.speedX = 0;
+  }
+  if (event.key == "w") {
+    animatedObject.speedY = 0;
+  }
+  if (event.key == "s") {
+    animatedObject.speedY = 0;
+  }
+})
+
+
+
+
 
 
 
---
+++ giocone/sprite.js
@@ -0,0 +1,12 @@
+var running = [
+    "img/Run__000.png",
+    "img/Run__001.png",
+    "img/Run__002.png",
+    "img/Run__003.png",
+    "img/Run__004.png",
+    "img/Run__005.png",
+    "img/Run__006.png",
+    "img/Run__007.png",
+    "img/Run__008.png",
+    "img/Run__009.png"
+];
